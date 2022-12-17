0de6e5d7c71b42edec2321db92a6ccbcb490a57a
Jenny <77757004+itsjenm@users.noreply.github.com>
2022-12-17 00:44:36 -0600

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+const list = document.querySelector("#ingredients")
+
+ingredients.forEach(Element => {
+  const items = document.createElement("li");
+  items.textContent = Element;
+  items.classList.add("item");
+  list.before(items)
+  console.log(items); 
+});
+
+
+
+
+
+
+
