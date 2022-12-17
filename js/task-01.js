ed5b9feb1f89d6e079789c95db0f58c46056307a
Jenny <77757004+itsjenm@users.noreply.github.com>
2022-12-16 22:21:00 -0600

    task 1 completed

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,12 @@
-const list = document.querySelectorAll("#categories");
-    console.log(list);
+function listCounter() {
+    const list = document.querySelector("#categories");
+    const listItems = document.querySelectorAll(".item");
+    console.log(`Number of categories: ${listItems.length}`);
+    
+    for (let i = 0; i < listItems.length; i++) {
+       console.log(`Category: ${listItems[i].querySelector("h2").innerText}`);
+       console.log(`Elements: ${listItems[i].getElementsByTagName("li").length}`);
+    }
+}
+
+listCounter();
