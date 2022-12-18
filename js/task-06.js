ff6827b52f317581f071aa65838ef297891f6fc5
Jenny <77757004+itsjenm@users.noreply.github.com>
2022-12-18 06:05:21 -0600

    task 6 completed

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const textInput = document.querySelector('#validation-input');
+
+
+
+textInput.addEventListener("blur", () => {
+   if (textInput.value.length >= textInput.getAttribute('data-length')) {
+    textInput.style.borderColor = "#4caf50";
+   } else {
+    textInput.style.borderColor = "#f44336";
+   }
+  
+});
+
+
+
+ 
