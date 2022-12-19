ccab8d66d9630f4d8b711d7458aa6668176d0c1e
Jenny <77757004+itsjenm@users.noreply.github.com>
2022-12-18 21:12:16 -0600

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+button.addEventListener("click", getRandomHexColor);
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.body.style.backgroundColor = randomColor; 
+  color.textContent = randomColor; 
+  return;
 }
+
+
